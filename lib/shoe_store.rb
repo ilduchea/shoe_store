b7550a65a61497818aa78c792fb250174f01b280
Ilduchea <ilduchea@gmail.com>
2017-05-12 14:07:17 -0700

    add vallidations and callbacks to ensure that brands and names are not longer than 100 characters and each word is capitalized. Passing

--- lib/shoe_store.rb
+++ lib/shoe_store.rb
@@ -1,9 +1,29 @@
 class Store < ActiveRecord::Base
   has_and_belongs_to_many(:shoes)
+  validates(:name, {:presence => true, :length => {:maximum => 100}})
+  before_save(:capitalize_each)
 
+private
+  def capitalize_each
+    names = name.split
+    names.each do |n|
+      n.capitalize!
+    end
+    self.name = names.join(' ')
+  end
 end
 
 class Shoe < ActiveRecord::Base
   has_and_belongs_to_many(:stores)
+  validates(:brand, {:presence => true, :length => {:maximum => 100}})
+  before_save(:capitalize_each)
 
+private
+  def capitalize_each
+    brands = brand.split
+    brands.each do |b|
+      b.capitalize!
+    end
+    self.brand = brands.join(' ')
+  end
 end
--- spec/shoe_store_spec.rb
+++ spec/shoe_store_spec.rb
@@ -2,8 +2,32 @@ require 'spec_helper'
 
 describe(Store) do
   it { should have_and_belong_to_many(:shoes)}
+
+  it { should validate_presence_of(:name)}
+
+  it 'ensures the length of name is a maximum of 100 characters' do
+    store = Store.new({:name => 'n'.*(101)})
+    expect(store.save).to eq(false)
+  end
+
+  it 'capitalizes the first letter of each word in the name' do
+    store = Store.create({:name => 'store name'})
+    expect(store.name).to eq('Store Name')
+  end
 end
 
 describe(Shoe) do
   it { should have_and_belong_to_many(:stores)}
+
+  it { should validate_presence_of(:brand)}
+
+  it 'ensures the length of brand is a maximum of 100 characters' do
+    shoe = Shoe.new({:brand => 'b'.*(101)})
+    expect(shoe.save).to eq(false)
+  end
+
+  it 'capitalizes the first letter of each word in the brand' do
+    shoe = Shoe.create({:brand => 'brand name'})
+    expect(shoe.brand).to eq('Brand Name')
+  end
 end
